5a04369daaa69220e28a73c952c817f7aa071ac4
abecassiskevin-dot <abecassis.kevin@icloud.com>
2025-10-19 13:56:38 -0400

    Add files via upload

---
+++ healthcheck_app.py
@@ -0,0 +1,7 @@
+# healthcheck_app.py
+import streamlit as st
+
+st.set_page_config(page_title="Healthcheck")
+st.title("✅ Healthcheck")
+st.write("OK — Streamlit démarre correctement!")
+st.success("Infra Streamlit Cloud fonctionne!")
