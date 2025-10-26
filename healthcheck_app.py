c495f65c123de9b49f21cb52490ce677d32a2fb7
abecassiskevin-dot <abecassis.kevin@icloud.com>
2025-10-26 17:17:55 -0400

    Delete healthcheck_app.py

--- healthcheck_app.py
+++
@@ -1,7 +0,0 @@
-# healthcheck_app.py
-import streamlit as st
-
-st.set_page_config(page_title="Healthcheck")
-st.title("✅ Healthcheck")
-st.write("OK — Streamlit démarre correctement!")
-st.success("Infra Streamlit Cloud fonctionne!")
